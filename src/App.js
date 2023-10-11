2144fb0ff2c017f87f7d6a443a1c42ae2e2011a1
Jinjeon Shin <sjg729729@gmail.com>
2023-10-12 00:50:07 +0900

    Add title, genre, image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div>시작 테스트</div>;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  async function getMovie() {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    );
+    const json = await response.json();
+    setLoading(false);
+    setMovies(json.data.movies);
+    console.log(json.data.movies);
+  }
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      <h1>Movie!!!</h1>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img
+                src={movie.medium_cover_image}
+                alt={movie.title + "'s Poster"}
+              ></img>
+              <div>{movie.title}</div>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
